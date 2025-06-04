6df4a7ce2308e95f4862f989a38a77ab226ee890
waleedhex <hexgames.22@gmail.com>
2025-06-04 21:48:12 +0300

    gift

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'hex-board-cache-v1';
+const urlsToCache = [
+    'hex-board.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
